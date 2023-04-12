094c9da3941dca9706bead9ce9b72363c617fcd5
Trinh Bac <trinhgiachang@gmail.com>
2023-04-12 21:21:17 +0700

    fix bug and clean code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey='1240791';
+const apiKey='v2nnk85w3hu7';
 const authToken = cookies.get("token");
 const client = StreamChat.getInstance(apiKey);
 
